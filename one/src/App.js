ad7d03e7a1b79612d3de0ae7c34ead04ce03dd0b
Karla Cervantes <cervantes.karla14@gmail.com>
2021-09-09 23:04:45 -0500

    added navbar

--- one/src/App.js
+++ one/src/App.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
